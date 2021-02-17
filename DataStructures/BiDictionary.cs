dd5fde7252a1f51229285231416ff70ef1886a71
Guriy Samarin <guriysamarin@outlook.com>
2021-02-17 09:16:39 +0300

    added BiDictionary (mostly by Jon Skeet)

---
+++ DataStructures/BiDictionary.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    public class BiDictionary<TFirst, TSecond> : IEnumerable
+    {
+        readonly IDictionary<TFirst, IList<TSecond>> _firstToSecond = new Dictionary<TFirst, IList<TSecond>>();
+        readonly IDictionary<TSecond, IList<TFirst>> _secondToFirst = new Dictionary<TSecond, IList<TFirst>>();
+
+        static readonly IList<TFirst> _emptyFirstList = Array.Empty<TFirst>();
+        static readonly IList<TSecond> _emptySecondList = Array.Empty<TSecond>();
+
+        public void Add(TFirst first, TSecond second)
+        {
+            IList<TFirst> firsts;
+            IList<TSecond> seconds;
+            if (!_firstToSecond.TryGetValue(first, out seconds))
+            {
+                seconds = new List<TSecond>();
+                _firstToSecond[first] = seconds;
+            }
+
+            if (!_secondToFirst.TryGetValue(second, out firsts))
+            {
+                firsts = new List<TFirst>();
+                _secondToFirst[second] = firsts;
+            }
+
+            seconds.Add(second);
+            firsts.Add(first);
+        }
+
+        public IList<TSecond> this[TFirst first] => GetByFirst(first);
+
+        public IList<TFirst> this[TSecond second] => GetBySecond(second);
+
+        public IList<TSecond> GetByFirst(TFirst first)
+        {
+            IList<TSecond> list;
+            if (!_firstToSecond.TryGetValue(first, out list))
+            {
+                return _emptySecondList;
+            }
+
+            return new List<TSecond>(list);
+        }
+
+        public IList<TFirst> GetBySecond(TSecond second)
+        {
+            IList<TFirst> list;
+            if (!_secondToFirst.TryGetValue(second, out list))
+            {
+                return _emptyFirstList;
+            }
+
+            return new List<TFirst>(list);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public IEnumerator GetEnumerator() => _firstToSecond.GetEnumerator();
+    }
+}
---
+++ XUnitTestProject/BiDictionaryTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using DataStructures;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class BiDictionaryTests
+    {
+        private BiDictionary<string, int> typesMap;
+
+        public BiDictionaryTests()
+        {
+            typesMap = new BiDictionary<string, int>
+            {
+                {"0", 0},
+                {"1", 1},
+                {"2", 2},
+                {"3", 3},
+                {"4", 4},
+                {"5", 5},
+                {"6", 6},
+                {"7", 7},
+                {"8", 8},
+                {"9", 9}
+            };
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        public void ShouldConverStringToInt(string stringNumber, int number) =>
+            Assert.Equal(stringNumber, typesMap[number].First());
+
+        [Theory]
+        [InlineData(1, "1")]
+        public void ShouldConvertIntToString(int number, string stringNumber) =>
+            Assert.Equal(number, typesMap[stringNumber].First());
+    }
+}

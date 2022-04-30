e839fa610ab19217658f6d8a0f9fe48c70ec7a43
Guriy Samarin <guriysamarin@outlook.com>
2022-04-30 15:00:35 +0300

    Create UnionFind.cs

---
+++ DataStructures/UnionFind.cs
@@ -0,0 +1,41 @@
+public class UnionFind
+{
+    private int[] root;
+
+    private int[] rank;
+
+    public UnionFind(int size)
+    {
+        root = new int[size];
+        rank = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public int Find(int x) => 
+    (x == root[x]) ? x : root[x] = find(root[x]);
+
+    public void Union(int x, int y)
+    {
+        int rootX = Find(x);
+        int rootY = Find(y);
+
+        if (rootX != rootY)
+        {
+            if (rank[rootX] >= rank[rootY])
+            {
+                root[rootY] = rootX;
+                rank[rootX] += rank[rootY];
+            }
+            else
+            {
+                root[rootX] = rootY;
+                rank[rootY] += rank[rootX];
+            }
+        }
+    }
+}

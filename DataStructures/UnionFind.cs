c0d7d895352186fee7c08b7d541a0a9ed5d13f6e
Guriy Samarin <guriysamarin@outlook.com>
2022-07-16 17:12:50 +0300

    invalidate size and small refactoring

--- DataStructures/UnionFind.cs
+++ DataStructures/UnionFind.cs
@@ -1,41 +1,36 @@
 public class UnionFind
 {
-    private int[] root;
-
-    private int[] rank;
+    private int[] _root;
+    private int[] _rank;
 
     public UnionFind(int size)
     {
-        root = new int[size];
-        rank = new int[size];
+        if (size <= 0) throw new ArgumentNullException(nameof(size));
+        _root = new int[size];
+        _rank = new int[size];
 
         for (int i = 0; i < size; i++)
-        {
-            root[i] = i;
-            rank[i] = 1;
-        }
+            (_root[i], _rank[i]) = (i, 1);
     }
 
     public int Find(int x) => 
-    (x == root[x]) ? x : root[x] = Find(root[x]);
+    (x == _root[x]) ? x : _root[x] = Find(_root[x]);
 
     public void Union(int x, int y)
     {
-        int rootX = Find(x);
-        int rootY = Find(y);
+        (int rootX, int rootY) = (Find(x), Find(y));
 
-        if (rootX != rootY)
+        if (rootX == rootY) return;
+        
+        if (rank[rootX] >= rank[rootY])
         {
-            if (rank[rootX] >= rank[rootY])
-            {
-                root[rootY] = rootX;
-                rank[rootX] += rank[rootY];
-            }
-            else
-            {
-                root[rootX] = rootY;
-                rank[rootY] += rank[rootX];
-            }
+            root[rootY] = rootX;
+            rank[rootX] += rank[rootY];
+        }
+        else
+        {
+            root[rootX] = rootY;
+            rank[rootY] += rank[rootX];
         }
     }
 }

5e3591d95eaf983903bc6ed991a29ab551e85658
Guriy Samarin <guriysamarin@outlook.com>
2023-04-07 11:25:28 +0100

    Update Program.cs

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,29 +1,29 @@
 ﻿using DataStructures;
 using System;
 
-namespace ConsoleUI
+namespace ConsoleUI;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
 
-            var heap = new Heap<int>((x, y) => x < y);
+        var heap = new Heap<int>((x, y) => x < y);
 
-            heap.Push(0);
-            Console.WriteLine(heap.Peek());
-            heap.Push(-3);
-            Console.WriteLine(heap.Peek());
-            heap.Push(20);
-            Console.WriteLine(heap.Peek());
-            heap.Push(330);
-            Console.WriteLine(heap.Peek());
-            heap.Push(10);
-            Console.WriteLine(heap.Peek());
-            heap.Push(1110);
-            Console.WriteLine(heap.Peek());
+        heap.Push(0);
+        Console.WriteLine(heap.Peek());
+        heap.Push(-3);
+        Console.WriteLine(heap.Peek());
+        heap.Push(20);
+        Console.WriteLine(heap.Peek());
+        heap.Push(330);
+        Console.WriteLine(heap.Peek());
+        heap.Push(10);
+        Console.WriteLine(heap.Peek());
+        heap.Push(1110);
+        Console.WriteLine(heap.Peek());
 
-        }
     }
 }
+

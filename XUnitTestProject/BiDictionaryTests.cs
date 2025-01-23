cfcf258b042d88b6d3db3b301eea1860b65f3515
Guriy Samarin <guriy@amazon.com>
2025-01-23 16:10:03 +0000

    converted to XUnit v3

--- XUnitTestProject/BiDictionaryTests.cs
+++ XUnitTestProject/BiDictionaryTests.cs
@@ -1,42 +1,41 @@
 ﻿using System.Linq;
 using DataStructures;
-using Xunit.v3;
+using Xunit;
 
 //https://stackoverflow.com/questions/255341/getting-multiple-keys-of-specified-value-of-a-generic-dictionary
 //https://stackoverflow.com/questions/268321/bidirectional-1-to-1-dictionary-in-c-sharp
 //https://stackoverflow.com/questions/10966331/two-way-bidirectional-dictionary-in-c
 
-namespace XUnitTestProject
+namespace XUnitTestProject;
+
+public class BiDictionaryTests
 {
-    public class BiDictionaryTests
+    private BiDictionary<string, int> typesMap;
+
+    public BiDictionaryTests()
     {
-        private BiDictionary<string, int> typesMap;
-
-        public BiDictionaryTests()
+        typesMap = new BiDictionary<string, int>
         {
-            typesMap = new BiDictionary<string, int>
-            {
-                {"0", 0},
-                {"1", 1},
-                {"2", 2},
-                {"3", 3},
-                {"4", 4},
-                {"5", 5},
-                {"6", 6},
-                {"7", 7},
-                {"8", 8},
-                {"9", 9}
-            };
-        }
-
-        [Theory]
-        [InlineData("1", 1)]
-        public void ShouldConverStringToInt(string stringNumber, int number) =>
-            Assert.Equal(stringNumber, typesMap[number].First());
-
-        [Theory]
-        [InlineData(1, "1")]
-        public void ShouldConvertIntToString(int number, string stringNumber) =>
-            Assert.Equal(number, typesMap[stringNumber].First());
+            {"0", 0},
+            {"1", 1},
+            {"2", 2},
+            {"3", 3},
+            {"4", 4},
+            {"5", 5},
+            {"6", 6},
+            {"7", 7},
+            {"8", 8},
+            {"9", 9}
+        };
     }
+
+    [Theory]
+    [InlineData("1", 1)]
+    public void ShouldConverStringToInt(string stringNumber, int number) =>
+        Assert.Equal(stringNumber, typesMap[number].First());
+
+    [Theory]
+    [InlineData(1, "1")]
+    public void ShouldConvertIntToString(int number, string stringNumber) =>
+        Assert.Equal(number, typesMap[stringNumber].First());
 }

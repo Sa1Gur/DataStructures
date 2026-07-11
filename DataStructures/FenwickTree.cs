ff1291a198e3788e9290bc8202da749a7de13eaf
Guriy Samarin <guriysamarin@outlook.com>
2026-07-12 00:59:17 +0300

    fix compilation FenwickTree.cs

--- DataStructures/FenwickTree.cs
+++ DataStructures/FenwickTree.cs
@@ -8,8 +8,10 @@ public class FenwickTree
     
     public FenwickTree(int size) => tree = new int[size + 1];
     
-    public void Update(int index, int value) =>
+    public void Update(int index, int value)
+    {
         for (++index; index < tree.Length; index += index & -index) tree[index] += value;
+    }
     
     public int Query(int left, int right) =>  PrefixSum(right) - (left > 0 ? PrefixSum(left - 1) : 0);
     private int PrefixSum(int index)
